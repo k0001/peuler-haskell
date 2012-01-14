05f7b36be6e7954760f3f4bc9f6c1bbc305c5e17
Renzo Carbonara <gnuk0001@gmail.com>
2012-01-14 20:08:54 -0300

    p6 e p6'

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Control.Applicative
 import Control.Arrow
+import Data.List (foldl')
 import System.CPUTime
 import System.Environment
 import System.Exit
@@ -77,23 +78,33 @@ intIsPalindrome n = n == intReverse n
 p1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 -- 233168
 
+
 p2 = sum [x | x <- takeWhile (<= 4000000) fibs, even x]
 -- 4613732
 
+
 p3 = last $ primeFactors 600851475143
 -- 6857
+--
 
 p4 = maximum $ filter intIsPalindrome $ (*) <$> [100..999] <*> [100..999]
 -- 906609
 
+
 p5 = foldl1 lcm [1..20]
 -- 232792560
 
-p6 = let (a, b) = (sum . map (^2)) &&& ((^2) . sum) $ [1..100]
-        in b - a
+
+p6 = let (a, b) = ((^2) . sum) &&& (sum . map (^2)) $ [1..100]
+        in a - b
 -- 25164150
 
+-- other approach using a monad to apply the same argument to more than one fun
+p6' = foldr (-) 0 $ [(^2) . sum, sum . map (^2)] >>= return . ($ [1..100])
+
+
 p7 = primes !! 10000
+-- 104743
 
 problems :: [Int]
 problems = [p1, p2, p3, p4, p5, p6, p7]
